c584b8dd509a5525185651d92c22fd9f6cd97f4e
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2021-10-09 20:37:34 +0200

    First Committ

---
+++ js/jq.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function(){
+  $(window).resize(function(){
+
+  
+   $(".x9").innerWidth($(".carousel").innerWidth());
+   $(".x9").innerHeight($(".carousel").innerHeight() + 80);
+  });
+  /*
+
+$(function() {  
+    $("body").niceScroll();
+});
+*/
+
+});
